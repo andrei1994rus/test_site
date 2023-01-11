1d309f1197d0b465fc4b08864d2949e3791eda12
andrei1994rus <rusakov.andrei@hotmail.com>
2023-01-11 20:40:16 +0300

    fix function serialize,refactor json.js,add cookie

--- json-site/js/json.js
+++ json-site/js/json.js
@@ -1,4 +1,4 @@
-var user=
+let user=
 {
     name:"Unnamed",
     age:26
@@ -6,27 +6,32 @@ var user=
 
 function serialize()
 {
-	var name_value=document.getElementById("name").value;
-	var age_value=document.getElementById("age").value;
+	let name_value=document.getElementById("name").value;
+	let age_value=document.getElementById("age").value;
+	
 	if(name_value=="")
 	{
 		console.log("Left text field has empty string.");
 		alert("Don't left empty string in left text field.");
 	}
+
 	if(age_value=="")
 	{
 		console.log("Right text field has empty string.");
 		alert("Don't left empty string in right text field.");
 	}
-	else
+
+	else if(name_value!=="" && age_value!=="")
 	{
 		user=
 		{
 		    name:name_value,
 		    age:age_value
 		};
-		var serializedUser=JSON.stringify(user);
+		let serializedUser=JSON.stringify(user);
+		document.cookie=`${serializedUser}`;
 		console.log(`serializedUser:${serializedUser}`);
+		console.log(`cookie: ${document.cookie}`);
 		document.getElementById("message").innerHTML=`<h1>Serialized user:${serializedUser}</h1>`;
 		$("#serialize_form [data-dismiss=modal]").trigger({ type: "click" });
 	}
@@ -34,8 +39,20 @@ function serialize()
 
 function parse()
 {
-	var serializedUser=JSON.stringify(user);
-	var parsedUser=JSON.parse(serializedUser);
-	console.log(`${parsedUser}`);
+	let serializedUser;
+
+	if(document.cookie)
+	{
+		serializedUser=document.cookie;
+		console.log(`from cookie: ${serializedUser}`);
+	}
+
+	else
+	{
+		serializedUser=JSON.stringify(user);
+	}
+
+	let parsedUser=JSON.parse(serializedUser);
+	console.log('parsedUser:',parsedUser);
 	document.getElementById("message").innerHTML=`<h1>Parsed name:${parsedUser.name};parsed age:${parsedUser.age}</h1>`;
 }
